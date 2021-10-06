15ff240be0460ccee6c867b5ae6b85830f379a94
Ivan Marín <imarinr@outlook.com>
2021-10-06 13:35:59 +0000

    Implementar el algoritmo de promedios en python

---
+++ implementacion_promedio.py
@@ -0,0 +1,32 @@
+# Este programa obtiene el promedio de 5 calificaciones dadas por el usuario
+# 06 de octubre de 2021
+# Autor: Ivan Marin
+
+# Algoritmo
+# 1. Inicio
+# 2. Definir cali1, cali2, cali3, cali4, cali5, promedio
+# 3. Imprimir "Dame la calificacion 1:"
+print("Dame la calificación 1:")
+# 4. hacer cali1 = entrada del usuario
+cali1 = float(input())
+# 5. Imprimir "Dame la calificacion 2:"
+print("Dame la calificación 2:")
+# 6. hacer cali2 = entrada del usuario
+cali2 = float(input())
+# 7. Imprimir "Dame la calificacion 3:"
+print("Dame la calificación 3:")
+# 8. hacer cali3 = entrada del usuario
+cali3 = float(input())
+# 9. Imprimir "Dame la calificacion 4:"
+print("Dame la calificación 4:")
+# 10. hacer cali4 = entrada del usuario
+cali4 = float(input())
+# 11. Imprimir "Dame la calificacion 5:"
+print("Dame la calificación 5:")
+# 12. hacer cali5 = entrada del usuario
+cali5 = float(input())
+# 13. hacer promedio = (cali1 + cali2 + cali3 + cali4 + cali5) / 5
+promedio = (cali1 + cali2 + cali3 + cali4 + cali5) / 5
+# 14. Imprimir "Dadas tus calificaciones, tu promedio seria de " promedio
+print("Dadas tus calificaciones, tu promedio seria de ", promedio)
+# 15. Fin
